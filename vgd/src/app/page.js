68041b6dcb70b0a241b69ef498880f819bc79676
Michael Torres <michael.46@live.it>
2024-11-20 13:56:10 +0100

    renaming all images for make works function

--- vgd/src/app/page.js
+++ vgd/src/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
             </div>
           </div>
 
-          {generateRows()}
+          {/* {generateRows()} */}
         </section>
 
         <section className="footer">
