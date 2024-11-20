93fcf4c397f95e8393018b3d85c25fb0ad3b7c52
Michael Torres <michael.46@live.it>
2024-11-20 13:42:40 +0100

    staring with function generating rows

---
+++ vgd/src/app/page.js
@@ -0,0 +1,67 @@
+"use client";
+import { useEffect } from "react";
+import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ReactLenis } from "lenis/react";
+
+export default function Home() {
+  const generateRows = () => {
+    const rows = [];
+    for (let i = 1; 1 <= 3; i++) {
+      rows.push(
+        <div className="row" key={i}>
+          <div className="card left-c">
+            <img src={`/img- ${2 * i - 1}.jpg`} alt="" />
+          </div>
+          <div className="card right-c">
+            <img src={`/img- ${2 * i}.jpg`} alt="" />
+          </div>
+        </div>
+      );
+    }
+    return rows;
+  };
+
+  return (
+    <>
+      <ReactLenis root>
+        <section className="landing-hero">
+          <img src="/Vincent.jpg" alt="Vincent Van Gogh" />
+        </section>
+        <section className="main">
+          <div className="main-content">
+            <div className="logo">
+              <img src="/Irises-1889-1.jpg" alt="" />
+            </div>
+            <div className="copy">
+              <div className="line">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+              </div>
+              <div className="line">
+                <p>
+                  Temporibus harum in debitis eum rem. A natus aperiam autem
+                </p>
+              </div>
+              <div className="line">
+                <p>
+                  Temporibus harum in debitis eum rem. A natus aperiam autem
+                </p>
+              </div>
+            </div>
+
+            <div className="button">
+              <button>Colorfull</button>
+            </div>
+          </div>
+
+          {generateRows()}
+        </section>
+
+        <section className="footer">
+          <Link href={"#"}>Link here</Link>
+        </section>
+      </ReactLenis>
+    </>
+  );
+}
