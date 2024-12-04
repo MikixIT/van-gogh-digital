cec90d758370945023bf77cbd5d34964015a1de1
Michael Torres <michael.46@live.it>
2024-12-04 17:43:24 +0100

    starting deploy fix2

--- vgd/next.config.mjs
+++ vgd/next.config.mjs
@@ -2,8 +2,9 @@
 // next.config.mjs
 const nextConfig = {
   assetPrefix: "/van-gogh-digital/vgd",
-  basePath: "/van-gogh-digital/vgd",
+  basePath: "/van-gogh-digital/vgd/",
   output: "export",
+  trailingSlash: true,
 };
 
 export default nextConfig;
