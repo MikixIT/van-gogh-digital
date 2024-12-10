e87c081f5a1eb6e1d532e326dc561240b8dd663b
Michael Torres <michael.46@live.it>
2024-12-10 01:25:35 +0100

    fix gitignore for ghpages

--- vgd/next.config.mjs
+++ vgd/next.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd ? "/" : "",
+  assetPrefix: isProd ? "/_next/" : "",
   images: {
     unoptimized: true,
   },
