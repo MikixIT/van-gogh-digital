813a2ff4f6f5794ed9b67283cbd632d5c821dc06
Michael Torres <michael.46@live.it>
2024-12-16 01:01:34 +0100

    github action disabled back to branch deploy classic

--- vgd/next.config.mjs
+++ vgd/next.config.mjs
@@ -1,8 +1,9 @@
 const nextConfig = {
-  assetPrefix: "/van-gogh-digital",
+  assetPrefix: "/van-gogh-digital/",
   images: {
     unoptimized: true,
   },
+  output: "export",
   basePath: "/van-gogh-digital",
   trailingSlash: true,
 };
